64d06cd796667cd0c36f75a781f7823576954d1c
poojadonawadi <138244294+poojadonawadi@users.noreply.github.com>
2025-01-07 23:07:30 +0530

    [Pooja] added: Employee Model class

---
+++ EmployeeModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePayroll_Ado.NET
+{
+    internal class EmployeeModel
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+    }
+}
